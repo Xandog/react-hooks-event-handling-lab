e0c23ae807d20971d7e13f72e3756de8bfbb1ef0
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-25 23:40:17 -0500

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default function Keypad() {
+    function handleChange(){
+        console.log("Entering password...");
+    }
+    
+    return (
+        <input type="password" onChange={handleChange}/>
+    )
+}
